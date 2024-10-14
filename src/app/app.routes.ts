fb7da6806d6ccb186fe76998460d3c5bd5118476
cristhian david <cvelas13@outlook.com>
2024-10-14 00:42:54 -0500

    implementado la navegación entre componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { MenuComponent } from './layout/menu/menu.component';
+import { LoginComponent } from './layout/login/login.component';
+import { PublicacionComponent } from './layout/publicacion/publicacion.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: MenuComponent
+    },
+    {
+        path: "inicio",
+        component: MenuComponent
+    },
+    {
+        path: "publicar",
+        component: PublicacionComponent
+    },
+    {
+        path: "login",
+        component: LoginComponent
+    }
+];
---
+++ src/app/layout/publicacion/publicacion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-publicacion',
+  standalone: true,
+  imports: [],
+  templateUrl: './publicacion.component.html',
+  styleUrl: './publicacion.component.css'
+})
+export class PublicacionComponent {
+
+}
