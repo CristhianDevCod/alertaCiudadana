541b3b115eb383434b97a83f6012ad63a871036f
cristhian david <cvelas13@outlook.com>
2024-10-20 22:35:14 -0500

    alineado de los campos del formulario

--- src/app/layout/publicacion/publicacion.component.ts
+++ src/app/layout/publicacion/publicacion.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-publicacion',
   standalone: true,
   imports: [],
-  templateUrl: './publicacion.component.html'
+  templateUrl: './publicacion.component.html',
+  styleUrl: './publicacion.component.css'
 })
 export class PublicacionComponent {
-  
+    
 }
