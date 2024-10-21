3b06df35596e13f5567c80344fd5317bb4e75375
cristhian david <cvelas13@outlook.com>
2024-10-21 07:20:49 -0500

    preparación del formulario para firebase

--- src/app/layout/publicacion/publicacion.component.ts
+++ src/app/layout/publicacion/publicacion.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { 
+  FormControl,
+  FormGroup, 
+  ReactiveFormsModule,
+  Validators 
+} from '@angular/forms';
 
 @Component({
   selector: 'app-publicacion',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './publicacion.component.html',
   styleUrl: './publicacion.component.css'
 })
 export class PublicacionComponent {
-    
+
+  noticiaForm = new FormGroup({
+    titulo: new FormControl('', Validators.required),
+    descripcion: new FormControl('', Validators.required),
+    imagen: new FormControl('', Validators.required)
+  });
+
+  onSubmit(){
+    console.log('funciona')
+  }
 }
