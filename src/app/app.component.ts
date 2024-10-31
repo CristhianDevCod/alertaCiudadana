dc31ca01a0ed7f8c2379fb34490db4053de630c5
Cristhian <cristhian19990302@gmail.com>
2024-10-31 11:23:43 -0500

    agregado apartado de footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
 import { MenuComponent } from "./layout/menu/menu.component";
+import { FooterComponent } from "./layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MenuComponent],
+  imports: [RouterOutlet, HeaderComponent, MenuComponent, FooterComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
